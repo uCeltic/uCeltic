c3f20cb756fa9da74b7f92a33a494d832c11ee8a
Dejian <jamiedz999@gmail.com>
2026-06-02 16:19:15 +0100

    test files adjustment

--- backend/.venv/bin/activate.fish
+++ backend/.venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/jamie/uCeltic/backend/.venv
+set -gx VIRTUAL_ENV /home/dejian/uCeltic/backend/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
